b92a9be2674fe74174621c1e90fec655e8eb81d8
Vladimir Petukhov <vladimir.petukhov@fastdev.se>
2017-03-22 15:48:26 +0500

    INITIAL: webpack.config.js + main project setting

---
+++ webpack.config.js
@@ -0,0 +1,99 @@
+let webpack = require('webpack');
+let path = require('path');
+let OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+let ExtractTextPlugin = require("extract-text-webpack-plugin");
+let extractCSS = new ExtractTextPlugin('css/[name].css');
+
+module.exports = {
+    entry: {
+        app: "./src/app.js",
+        vendor: [
+            'vue',
+            'axios',
+            './bower_components/bootstrap/dist/js/bootstrap.js',
+            './bower_components/bootstrap/dist/css/bootstrap.css'
+        ],
+    },
+    output: {
+        path: path.resolve(__dirname, 'public/assets'),
+        filename: 'js/[name].js',
+        publicPath: './public'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /(node_modules)/,
+                options: {
+                    presets: ['es2015']
+                }
+            },
+            {
+                test: /\.css$/,
+                use: extractCSS.extract({
+                    fallback: "style-loader",
+                    use: "css-loader"
+                })
+            },
+            {
+                test: /\.(woff|woff2)$/,
+                loader: 'url-loader?limit=10000'
+            },
+            {
+                test: /\.ttf$/,
+                loader: 'file-loader'
+            },
+            {
+                test: /\.eot$/,
+                loader: 'file-loader'
+            },
+            {
+                test: /\.svg$/,
+                loader: 'file-loader'
+            }
+        ]
+
+    },
+    resolve: {
+        alias: {
+            'vue$': 'vue/dist/vue.common.js',
+            jquery: "bower_components/jquery/dist/jquery.js",
+        }
+    },
+    plugins: [
+        extractCSS,
+        new webpack.optimize.CommonsChunkPlugin({
+            names: ['vendor']
+        }),
+        new OptimizeCssAssetsPlugin({
+            // assetNameRegExp: /\.optimize\.css$/g,
+            cssProcessor: require('cssnano'),
+            cssProcessorOptions: {
+                discardComments: {
+                    removeAll: true
+                }
+            },
+            canPrint: true
+        })
+    ]
+};
+
+if (process.env.NODE_ENV === 'production') {
+    module.exports.plugins.push(
+        new webpack.optimize.UglifyJsPlugin({
+            sourcemap: true,
+            compress: {
+                warnings:false
+            }
+        })
+    );
+
+    module.exports.plugins.push(
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: 'production'
+            }
+        })
+    )
+}
